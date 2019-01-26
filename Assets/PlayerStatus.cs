15c9f894f04e7008df954828b24240925376ba0a
Pablo Miceli <pablomiceli24@gmail.com>
2019-01-25 22:31:16 -0300

    Player status

---
+++ Assets/PlayerStatus.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public float health = 100f;
+    public float stamina = 100f;
+    public int batteries = 0;
+    public bool running = false;
+    public bool recharging = false;
+}
