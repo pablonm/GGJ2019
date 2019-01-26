1ae9dd8dfb4f70479fcba6734e3d3df1e81ce8cd
Tono ;) <tonoezep@gmail.com>
2019-01-26 04:01:30 -0300

    searchable object script

---
+++ Assets/GlobalSettings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GlobalSettings : MonoBehaviour
+{
+    public float vodkaRate = 2f;
+    public float betteryRate = 1f;
+    public float maxStamina = 100f;
+    public int maxBattery = 8;
+}
---
+++ Assets/SearchableObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchableObject : MonoBehaviour
+{
+    public float rate;
+
+    private bool isOpened;
+    private PlayerStatus status;
+    private GlobalSettings globalSettings;
+    private Random randomer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpened = false;
+        status = FindObjectOfType<PlayerStatus>();
+        globalSettings = FindObjectOfType<GlobalSettings>();
+        Random.InitState(System.DateTime.Now.Millisecond);
+    }
+
+    private void DiscoverRandomObject()
+    {
+        isOpened = true;
+        status.batteries += Mathf.FloorToInt(Random.Range(0f, globalSettings.betteryRate));
+        if (status.batteries > globalSettings.maxBattery)
+        {
+            status.batteries = globalSettings.maxBattery;
+        }
+        status.stamina += Mathf.FloorToInt(Random.Range(0f, globalSettings.vodkaRate));
+        if (status.stamina > globalSettings.maxStamina)
+        {
+            status.stamina = globalSettings.maxStamina;
+        }
+        Debug.Log(status.batteries);
+        Debug.Log(status.stamina);
+    }
+
+    private void OnTriggerStay2D(Collider2D colider)
+    {
+        if (!isOpened && (colider.tag == "Player") && Input.GetKeyDown(KeyCode.Space))
+        {
+            DiscoverRandomObject();
+        }
+    }
+}
