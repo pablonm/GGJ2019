c0620e48615aac9ae90232fcd070b41b486534d2
Pablo Miceli <pablomiceli24@gmail.com>
2019-01-26 10:13:12 -0300

    Progress on house

---
+++ Assets/Scripts/ActionableObject.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ActionableObject : MonoBehaviour
+{
+    public enum Buttons { A, LT }
+    public Buttons button;
+    public string message;
+    protected bool done = false;
+
+    private GameObject actionMessagePrefab;
+
+    protected abstract void Action();
+    protected abstract void OnEnter();
+    protected abstract void OnExit();
+
+    private void Awake()
+    {
+        LoadResources();
+    }
+
+    private void LoadResources() {
+        actionMessagePrefab = Resources.Load<GameObject>("ActionMessage");
+        Sprite buttonSprite = null;
+        switch (button) {
+            case Buttons.A:
+                buttonSprite = Resources.Load<Sprite>("A");
+                break;
+            case Buttons.LT:
+                buttonSprite = Resources.Load<Sprite>("LT");
+                break;
+        }
+        actionMessagePrefab = Instantiate(actionMessagePrefab, transform);
+        actionMessagePrefab.transform.Find("Message").GetComponent<Text>().text = message;
+        actionMessagePrefab.transform.Find("Button").GetComponent<Image>().sprite = buttonSprite;
+        actionMessagePrefab.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!done && collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space))
+        {
+            done = true;
+            actionMessagePrefab.SetActive(false);
+            Action();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!done && collision.gameObject.CompareTag("Player"))
+        {
+            actionMessagePrefab.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!done && collision.gameObject.CompareTag("Player"))
+        {
+            actionMessagePrefab.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/GrabFlashlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GrabFlashlight : ActionableObject
+{
+    public SpriteRenderer sprite;
+    public Sprite normalFlashlight;
+    public Sprite highlightFlashlight;
+
+    private void Start()
+    {
+        Debug.Log("ASDASD");
+    }
+
+    protected override void Action()
+    {
+        // NADA
+    }
+
+    protected override void OnEnter()
+    {
+        // NADA
+    }
+
+    protected override void OnExit()
+    {
+        // NADA
+    }
+}
