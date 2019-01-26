0758e14d2377be955da12ca760842bc3a98251e9
Tono ;) <tonoezep@gmail.com>
2019-01-26 15:22:59 -0300

    Enemy damage

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private float health;
+
+    void Start()
+    {
+        health = GlobalSettings.maxEnemyLife;
+    }
+
+    private void TakeDamage()
+    {
+        Debug.Log(health);
+
+        health -= Time.deltaTime * GlobalSettings.lightDamage;  
+
+         if (health < 0)
+        {
+            Debug.Log("Mori xD");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Light"))
+        {
+            TakeDamage();
+        }
+    }
+}
--- Assets/Scripts/GlobalSettings.cs
+++ Assets/Scripts/GlobalSettings.cs
@@ -7,4 +7,6 @@ public static class GlobalSettings
     public static float maxStamina = 100f;
     public static int maxBattery = 8;
     public static Transform cameraTarget;
+    public static float maxEnemyLife = 100f;
+    public static float lightDamage = 10f;
 }
