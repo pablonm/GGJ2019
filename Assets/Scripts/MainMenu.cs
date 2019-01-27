3d39db2e6d618c67e4501296e7c61414b176ac7a
Pablo Miceli <pablomiceli24@gmail.com>
2019-01-27 10:04:29 -0300

    Que se yo

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject playIndicator;
+    public GameObject exitIndicator;
+    private bool play = true;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0) {
+            playIndicator.SetActive(true);
+            exitIndicator.SetActive(false);
+            play = true;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            playIndicator.SetActive(false);
+            exitIndicator.SetActive(true);
+            play = false;
+        }
+        if (Input.GetAxis("Action") > 0) {
+            if (play)
+                SceneManager.LoadScene("Game");
+            else
+                Application.Quit();
+        }
+    }
+}
