b66fb700e6ef05aff6569db175854421dcdbd142
Pablo Miceli <pablomiceli24@gmail.com>
2019-01-27 04:07:41 -0300

    qwe

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,13 +28,27 @@ public class GameManager : MonoBehaviour
     }
 
     private void Init() {
+
+        /* Settings */
         GlobalSettings.betteryRate = 2f;
         GlobalSettings.vodkaRate = 20f;
         GlobalSettings.maxBattery = 8;
         GlobalSettings.cameraTarget = cameraTarget;
         GlobalSettings.EnemyDebuff = EnemyDebuff;
-        bgm.PlayClips(new List<PlayClipInfo>() { new PlayClipInfo("calma", true) });
+
+        /* Player status */
         playerStatus.transform.position = initialPlayerPosition;
+        playerStatus.health = 100f;
+        playerStatus.currentBatery = 100f;
+        playerStatus.batteries = 0;
+        playerStatus.stamina = 100f;
+        playerStatus.blockMovement = false;
+        playerStatus.running = false;
+        playerStatus.recharging = false;
+        playerStatus.usingFlashlight = false;
+        playerStatus.grabbingChild = false;
+
+        bgm.PlayClips(new List<PlayClipInfo>() { new PlayClipInfo("calma", true) });
         Camera.main.transform.position = new Vector3(initialPlayerPosition.x, initialPlayerPosition.y, Camera.main.transform.position.z);
         StartCoroutine(initDialog());
     }
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Image battery;
+    public Text batteryCount;
+    public Image vodka;
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
+
+    private PlayerStatus status;
+
+    private void Start()
+    {
+        status = FindObjectOfType<PlayerStatus>();
+    }
+
+    private void Update()
+    {
+        battery.fillAmount = status.currentBatery * 0.01f;
+        batteryCount.text = "x " + status.batteries.ToString();
+        vodka.fillAmount = status.stamina * 0.01f;
+        CheckLives();
+    }
+
+    private void CheckLives() {
+        life1.SetActive(status.health > 1f);
+        life2.SetActive(status.health > 34f);
+        life3.SetActive(status.health > 67f);
+    }
+}
